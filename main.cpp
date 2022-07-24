d1791dd0c0f19963d11078fa1401433ad698c455
מיכל יעקובשוילי <michal.iakobashvili@ashdod.eduil.org>
2022-07-24 20:15:55 +0300

    hohoho

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World! Bitches in the house" << std::endl;
+    std::cout << "Hello, World! Bitches in the house lol" << std::endl;
+   
     return 0;
 }
